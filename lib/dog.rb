d6da07d6a0796fc1ef335458ab4cc95c522fdfb3
kimalibennett <aminbennett@gmail.com>
2020-12-02 12:48:16 -0600

    commit tree 4b0e4ed03833990c97dbb61f10502d63420450d2

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,26 @@
-# Add your code here
+class Dog
+    attr_accessor :name
+    
+    @@all = []
+    
+    def initialize(name)
+      @name = name
+      @@all << self
+    end
+    
+    def self.all
+      @@all.each do |dog|
+        puts dog.name
+      end
+    end
+    
+    def Dog.clear_all
+      @@all.clear
+    end
+
+    def Dog.print_all
+        @@all.each do |dog|
+            puts dog.name
+    end
+    
+end
